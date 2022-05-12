166e678e68d59e21d2e4e76402a96e89ead943f2
Cam <camwolff02@gmail.com>
2022-05-12 00:17:16 -0700

    Update megabot.py

--- Megabot_py/megabot.py
+++ Megabot_py/megabot.py
@@ -8,7 +8,7 @@ logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
-
+    
 class Megabot(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
